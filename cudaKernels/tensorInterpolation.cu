254c86c6f827ee8eb55de21ec70e6dcfc38611e7
ichlubna <ichlubna@fit.vutbr.cz>
2022-10-10 17:22:49 +0200

    Working tensor example backup

--- cudaKernels/tensorInterpolation.cu
+++ cudaKernels/tensorInterpolation.cu
@@ -20,30 +20,46 @@ __device__ void interpolateImages(Images images, unsigned char *result, half wei
     constexpr int MAT_PX_COUNT{8}; 
     constexpr int MAT_VIEW_COUNT{16};
     constexpr int CHANNELS{4};
-    __shared__ half pixelMatrix[256/WARP_SIZE][MAT_PX_COUNT*CHANNELS][MAT_VIEW_COUNT];
-    int warpID = (blockIdx.x*blockDim.y)/WARP_SIZE;
+
+    int warpID = threadIdx.x/WARP_SIZE;
     uint2 pxCoords{coords.x/CHANNELS, coords.y};
-    int channelID = coords.x%CHANNELS;
+    int channelID = threadIdx.x%CHANNELS;
     //int matrixRowID = CHANNELS*((int)(coords.x%WARP_SIZE)/CHANNELS) + channelID;
-    int matrixRowID =coords.x%WARP_SIZE;
+    int matrixRowID = threadIdx.x%WARP_SIZE;//coords.x%WARP_SIZE;
     float2 gridCenter{(GRID_COLS-1)/2.f, (GRID_ROWS-1)/2.f};
-     
+
+    __shared__ half pixelMatrix[256/WARP_SIZE][MAT_PX_COUNT*CHANNELS][MAT_VIEW_COUNT];
+ 
+    __shared__ half localWeights[WEIGHTS_ROWS][WEIGHTS_COLS];
+    int *intLocal = reinterpret_cast<int*>(localWeights[0]);
+    int *intIn = reinterpret_cast<int*>(weights[0]);
+    intLocal[threadIdx.x] = intIn[threadIdx.x]; 
+    
+    __shared__ half localWeightSums[WEIGHTS_ROWS];
+    intLocal = reinterpret_cast<int*>(localWeightSums);
+    intIn = reinterpret_cast<int*>(weightSums);
+    if(threadIdx.x < 4) 
+        intLocal[threadIdx.x] = intIn[threadIdx.x]; 
+ 
+    __syncthreads();
+
     wmma::fragment<wmma::accumulator, 32, 8, 16, half> matResult;
     wmma::fill_fragment(matResult, 0.0f);
     wmma::fragment<wmma::matrix_a, 32, 8, 16, half, wmma::row_major> matPixels;
     wmma::fragment<wmma::matrix_b, 32, 8, 16, half, wmma::row_major> matWeights;
 
-    int batchCount = __float2int_ru((float)(GRID_ROWS*GRID_ROWS)/MAT_VIEW_COUNT);
+//wmma::fill_fragment(matWeights, 1.0f/64);
+    int batchCount = 4;//__float2int_ru((float)(GRID_ROWS*GRID_ROWS)/MAT_VIEW_COUNT);
     for(int i=0; i<batchCount; i++)
     {
-        wmma::load_matrix_sync(matWeights, weights[i*MAT_VIEW_COUNT], MAT_PX_COUNT);
+        wmma::load_matrix_sync(matWeights, localWeights[i*MAT_VIEW_COUNT], MAT_PX_COUNT);
 
         for(int j=0; j<MAT_VIEW_COUNT; j++)
         {
             int gridID = i*MAT_VIEW_COUNT+j; 
-            int2 focusedCoords = focusCoords(pxCoords, 10, {gridID%GRID_COLS, gridID/GRID_COLS}, gridCenter);
-            auto pixel = images.getPixelAsArray<float>(gridID, focusedCoords);
-            pixelMatrix[warpID][matrixRowID][gridID] = pixel[channelID]; 
+            int2 focusedCoords = focusCoords(pxCoords, 10, {(unsigned int)gridID/GRID_COLS, (unsigned int)gridID%GRID_COLS}, gridCenter);
+            auto pixel = images.getPixelAsArray<half>(gridID, focusedCoords);
+            pixelMatrix[warpID][matrixRowID][j] = pixel[channelID]; 
         //ZKUSIT BEZ SHARED
         //for(int j=0; j<4; j++)
           //      sum[j] = matAcc.x[0];
@@ -52,9 +68,9 @@ __device__ void interpolateImages(Images images, unsigned char *result, half wei
         wmma::mma_sync(matResult, matPixels, matWeights, matResult);
     }
     
-    __shared__ half resultMatrix[256/WARP_SIZE][MAT_PX_COUNT*CHANNELS][MAT_PX_COUNT];
-    wmma::store_matrix_sync(resultMatrix[warpID][0], matResult, 0, wmma::mem_row_major);
+    __shared__ half resultMatrix[256/WARP_SIZE][MAT_PX_COUNT*CHANNELS][8];
+    wmma::store_matrix_sync(resultMatrix[warpID][0], matResult, 8, wmma::mem_row_major);
     
     //for all views udelat 
-    images.rawOutData[coords.y*IMG_WIDTH*CHANNELS + coords.x] = (unsigned int)(resultMatrix[warpID][matrixRowID][0]/weightSums[0]); 
+    images.rawOutData[coords.y*IMG_WIDTH*CHANNELS + coords.x] = (unsigned int)(resultMatrix[warpID][matrixRowID][0]/localWeightSums[0]); 
 }
