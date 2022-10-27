e69dee78bca4cf005d5c1134f6ee17955ad6ea67
ichlubna <ichlubna@fit.vutbr.cz>
2022-10-27 10:55:07 +0200

    Cleanup

--- pixeltools.py
+++
@@ -1,9 +0,0 @@
-class PixelTools:
-    def splitChannels(self, pixels):
-        channels = []
-        for channel in pixels[0]:
-            channels.append([])
-        for pixel in pixels:
-            for i in range(len(pixel)):
-                channels[i].append(pixel[i])
-        return channels
